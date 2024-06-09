c3db14c6a8c5cf8d1c1a812605411b9e48db7b58
fastbooteraselk <141639047+eraselk@users.noreply.github.com>
2024-06-09 23:50:52 +0800

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -9,10 +9,7 @@ export IS_MASTER="${1}"
 download() {
     sudo apt update -y && sudo apt upgrade -y && sudo apt-get install -y flex bison ncurses-dev texinfo gcc gperf patch libtool automake g++ libncurses5-dev gawk subversion expat libexpat1-dev python-all-dev binutils-dev bc libcap-dev autoconf libgmp-dev build-essential pkg-config libmpc-dev libmpfr-dev autopoint gettext txt2man liblzma-dev libssl-dev libz-dev mercurial wget tar gcc-10 g++-10 zstd glibc-source libc6-dev-i386 --fix-broken --fix-missing
     git clone -b binutils-2_42 --depth=1 git://sourceware.org/git/binutils-gdb.git binutils
-    wget https://ftp.gnu.org/gnu/gcc/gcc-11.1.0/gcc-11.1.0.tar.gz -O gcc-tarball
-    tar -xf gcc-tarball
-    rm -f gcc-tarball
-    mv -f gcc* gcc
+    git clone -b releases/gcc-4.9.0 --depth=1 git://gcc.gnu.org/git/gcc.git gcc
     git clone -b v1.5.5 https://github.com/facebook/zstd zstd
   sed -i '/^development=/s/true/false/' binutils/bfd/development.sh
   cd gcc
